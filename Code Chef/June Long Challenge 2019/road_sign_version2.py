38684cf2c5d29d61c8b4e9a54438e4d4369630d2
GuptaAman08 <2015aman.a@ves.ac.in>
2019-06-28 09:52:19 +0530

    Changes made in Road Sign problems

--- Code Chef/June Long Challenge 2019/road_sign_version2.py
+++ Code Chef/June Long Challenge 2019/road_sign_version2.py
@@ -1,10 +1,13 @@
 
 t=int(input())
 
+
+
 for _ in range(t):
     k = int(input())
     mod = 1000000007
     ans = pow(2, (k-1), mod)
-    print(ans * 10)
-
+    ans = ans * 10
+    ans = ans % 1000000007
+    print(ans)
     
