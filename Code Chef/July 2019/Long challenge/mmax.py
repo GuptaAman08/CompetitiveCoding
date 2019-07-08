d0eedf625dff2286cca68f5a57f29d4d42e29059
GuptaAman08 <2015aman.a@ves.ac.in>
2019-07-08 14:03:04 +0530

    Finally Solved 2nd problem

--- Code Chef/July 2019/Long challenge/mmax.py
+++ Code Chef/July 2019/Long challenge/mmax.py
@@ -1,32 +1,28 @@
 t = int(input())
 
 while t != 0:
-    try:
-        n = int(input())
-        k = int(input())
+    n = int(input())
+    k = int(input())
 
-        permut = []
+    sum_max = 0
+    if ((n < k) and (k%n) == 0) or (n==k):
         sum_max = 0
-        if ((n < k) and (k%n) == 0) or (n==k):
-            sum_max = 0
-        elif n > k: 
-            if k > (n-k):
-                sum_max = 2 * (n-k)
-            elif k==(n-k):
-                sum_max = (n//2) + ((n//2) - 1)
-            else:
-                sum_max = 2 * k
+    elif n > k: 
+        if k > (n-k):
+            sum_max = 2 * (n-k)
+        elif k==(n-k):
+            sum_max = (n//2) + ((n//2) - 1)
         else:
-            a = int(k/n)
-            b = k%n
-            
-            if b > (n-b):
-                sum_max = 2 * (n-b)
-            elif (b==(n-b)):
-                sum_max = (n//2) + ((n//2) - 1)
-            else:
-                sum_max = 2 * b
-        t -= 1
-        print(sum_max)
-    except Exception as e:
-        print(e)
+            sum_max = 2 * k
+    else:
+        b = k%n
+        
+        if b > (n-b):
+            sum_max = 2 * (n-b)
+        elif (b==(n-b)):
+            sum_max = (n//2) + ((n//2) - 1)
+        else:
+            sum_max = 2 * b
+    t -= 1
+    print(sum_max)
+    
