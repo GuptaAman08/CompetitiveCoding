20b840f0beb159cb57adec77f3a03e0a38556233
GuptaAman08 <2015aman.a@ves.ac.in>
2019-08-01 09:48:44 +0530

    printing all subarrays in n^2 time

--- Hacker Rank/other/Problems on SubArray/sub_array_ver2.py
+++ Hacker Rank/other/Problems on SubArray/sub_array_ver2.py
@@ -3,11 +3,13 @@ n = int(input())
 a = list(map(int, input().split()))
 
 # This is based on combinatoric link:https://www.quora.com/What-is-the-most-efficient-way-of-finding-all-the-subarrays-from-an-array-of-integers
-
+# O(N^2)
 # Here we make selection for start(i) and end(j) indices such that i<=j
 start = time()
 for i in range(n):
-    for j in range(i,n):
-        print(*a[i:j+1])
+	s = ""
+	for j in range(i,n):
+		s = s + str(a[j])
+		print(s)
 end = time()
 print(end - start)
