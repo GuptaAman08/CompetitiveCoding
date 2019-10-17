ee4ebd176ebc3345952d1c4d3adb514ff6f99f4b
GuptaAman08 <2015aman.a@ves.ac.in>
2019-10-17 10:32:58 +0530

    Fimally solved 4th problem

--- Code Chef/October 2019/Long Challenge/msv.py
+++ Code Chef/October 2019/Long Challenge/msv.py
@@ -1,5 +1,16 @@
-from sys import stdin
 import math
+from sys import stdin
+
+
+def cal_multiples(n):
+    sqrt_n = int(math.sqrt(n)) + 1
+    for i in range(1, sqrt_n):
+        if n % i == 0:
+            d[i] += 1
+            temp = n//i
+            if temp != i:
+                d[temp] += 1
+
 
 t = int(stdin.readline())
 
@@ -7,22 +18,15 @@ while t:
     n = int(stdin.readline())
 
     a = [int(x) for x in stdin.readline().split()]
-    d = defaultdict(int)
-    
+
+    d = [0]*((10**6)+1)
+
     max_c = 0
     for i in range(1, n):
-        sqrt_n = int(math.sqrt(a[i-1]))+1
-        for j in range(1, sqrt_n):
-            if a[i-1] % j == 0:
-                d[j] += 1
-                temp = a[i-1]//j
-                if temp != j:
-                    d[temp] += 1
-        
+        cal_multiples(a[i-1])
         if d[a[i]] > max_c:
             max_c = d[a[i]]
-        
-    
+
     print(max_c)
-    
+
     t -= 1
