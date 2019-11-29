7c7af7f73b4996430cddeb6249b4c73ab71c53db
GuptaAman08 <2015aman.a@ves.ac.in>
2019-11-29 09:52:13 +0530

    finding whether there exist a pair with a given sum using two pointer concept

---
+++ Other Company Coding Problem/twoPointerConcept.py
@@ -0,0 +1,28 @@
+# Find whether there exist a pair in a sorted array that has a given sum
+# here we use two pointer technique
+# Link: https://www.youtube.com/watch?v=OnaYh_naEAE
+
+n = int(input())
+sort_arr = [int(x) for x in input().split()]
+
+givenSum = int(input())
+
+# two pointers.......
+i, j = 0, n-1
+f = 0
+
+while i<j:
+    if (sort_arr[i] + sort_arr[j]) < givenSum:
+        i += 1
+    elif (sort_arr[i] + sort_arr[j]) < givenSum:
+        j -= 1
+    else:
+        f = 1
+        break
+
+if f:
+    print('Yes')
+else:
+    print('No')
+
+
